7cc1397094e3c09ff28d88201f9bdd2481fbf668
Aleksander Sosna <aleksander.sosnatree@gmail.com>
2023-11-15 14:42:58 +0100

    productId with letter needed toLowerCase()

--- scripts/content.js
+++ scripts/content.js
@@ -1,7 +1,7 @@
 const productWrapperSelector = document.querySelector(
   "div.pip-page-container__main > div[data-product-id]"
 );
-const productId = productWrapperSelector.getAttribute("data-product-id");
+const productId = productWrapperSelector.getAttribute("data-product-id").toLowerCase();
 const productNo = productWrapperSelector.getAttribute("data-product-no");
 const productPrice = Number(productWrapperSelector.getAttribute("data-product-price"));
 const countryCode = document.URL.split("https://www.ikea.com/")[1].split(
